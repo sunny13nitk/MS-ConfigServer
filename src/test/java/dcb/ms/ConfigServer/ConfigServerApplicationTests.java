9691c61773c8b60f47c051ae51824a1640072ca9
Sunny Bhardwaj <sunny13nitk@gmail.com>
2023-03-31 15:26:56 +0530

    Config server - Git

---
+++ src/test/java/dcb/ms/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dcb.ms.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
